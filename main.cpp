d3f77488df3432222d83e74d506a832f4b7425f4
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-06-19 12:51:14 +0300

    Add figures cells

--- main.cpp
+++ main.cpp
@@ -1,5 +1,23 @@
 #include <SFML/Graphics.hpp>
 
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = {0};
+
+struct { int x; int y; } buffer_a[4], buffer_b[4];
+
+// Types of figures (cells in 2x4 field)
+int figures[7][4] = {
+    1, 3, 5, 7,  // I
+    2, 4, 5, 7,  // Z
+    3, 5, 4, 6,  // S
+    3, 5, 4, 7,  // T
+    2, 3, 5, 7,  // L
+    3, 5, 7, 6,  // J
+    2, 3, 4, 5   // O
+};
+
 
 int main()
 {
