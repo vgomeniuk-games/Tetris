ba3c401ba7846231a50877fd456a1d22b86e96bf
vgomeniuk <vgomeniuk@lvivsoft.com>
2017-06-19 12:42:55 +0300

    Create window and load tile sprite

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(320, 480), "Tetris");
+
+    // Load textures
+    sf::Texture tTiles;
+    tTiles.loadFromFile("assets/tiles.png");
+
+    // Create sprites from textures
+    sf::Sprite sTiles(tTiles);
+    sTiles.setTextureRect(sf::IntRect(0, 0, 18, 18));  // TODO
+
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event e;
+        while (window.pollEvent((e))) {
+            if (e.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+        window.draw(sTiles);
+        window.display();
+    }
 
     return 0;
 }
